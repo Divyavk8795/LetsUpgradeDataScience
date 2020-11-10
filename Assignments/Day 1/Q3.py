dbc60e80043b9aee9840aa9a834a0f879549c9d1
Divyavk8795 <69857712+Divyavk8795@users.noreply.github.com>
2020-11-10 13:21:55 +0530

    Add files via upload

---
+++ Assignments/Day 1/Q3.py
@@ -0,0 +1,8 @@
+x=input()   #cost price
+y=input()   #selling price
+if x>y:
+   print("Loss")
+elif y>x:
+    print("Profit")
+else :
+    print("Neither")       
---
+++ Assignments/Day 1/Q4.py
@@ -0,0 +1,3 @@
+x=int(input())
+rs=x*80
+print(rs)
